7877e7d0b3f90beedc2d2884b1c434b1eebbcf89
Mukunthan A R <mukunthan.1255@gmail.com>
2025-03-24 11:03:55 +0530

    refactor code.

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function setScore(score) {
-    const needle = document.querySelector('.needle');
-    // Convert score to degrees
-    // 0 score = -90deg, 100 score = 90deg
-    const degrees = -90 + (score * 1.8); // 1.8 = 180/100 to spread 180 degrees
-    needle.style.transform = `translateX(-50%) rotate(${degrees}deg)`;
-}
-
-
